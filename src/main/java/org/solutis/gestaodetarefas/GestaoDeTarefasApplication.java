437d54aa2fc99b2fa6b0a95e85b5bb95e768673d
Gui <oxemtex@gmail.com>
2025-04-05 13:25:14 -0300

    Bug fix CORS

--- src/main/java/org/solutis/gestaodetarefas/GestaoDeTarefasApplication.java
+++ src/main/java/org/solutis/gestaodetarefas/GestaoDeTarefasApplication.java
@@ -1,9 +1,11 @@
 package org.solutis.gestaodetarefas;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
+@OpenAPIDefinition(servers = {@Server(url = "/",  description = "Default Server URl")})
 @SpringBootApplication
 
 public class GestaoDeTarefasApplication {
